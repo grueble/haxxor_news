d637564bf02cee432a4a0e0e568306abf7758dea
Will Gueble <will.gueble@viget.com>
2013-06-10 11:11:09 -0600

    Initial Commit - Base App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HaxxorNews::Application.config.secret_token = '4605b9869d5b5a9c83cf87edf6bf64d70016d72e2ca4f403720b3e1f7883ee5ae5f6200a8ff471031b1506a967817af20c482b9771cab36faa95b5e0a23c2580'
