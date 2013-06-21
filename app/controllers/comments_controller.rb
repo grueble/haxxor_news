ed2b2f6dd099571827c7f0dd7f7bd07e8ebae5f9
Will Gueble <will.gueble@viget.com>
2013-06-21 11:43:41 -0600

    Add Several New Tests and Increase Safety of Code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,20 @@
 class CommentsController < ApplicationController
   before_filter :login_required, :only => [ :create ]
-  before_filter :load_commentable, :only => [ :create ]
   
   def show
     @comment = Comment.find(params[:id])
   end
   
   def create
-    @comment = current_user.comments.build(params[:comment])
-    if @comment.save
-      redirect_to @commentable, :notice => "You have successfully posted a comment"
+    comment = current_user.comments.build(params[:comment])
+    if comment.save
+      redirect_to comment.commentable, :notice => "You have successfully posted a comment"
     else
-      redirect_to @commentable, :alert => "Comment must have text"
+      if comment.commentable
+        redirect_to comment.commentable, :alert => "Comment must have text"
+      else
+        redirect_to root_url, :alert => "We weren't able to save your comment at this time"
+      end
     end
-  end
-  
-  private
-  
-  def load_commentable
-    @commentable = params[:comment][:commentable_type].constantize.find(params[:comment][:commentable_id])
   end
 end
