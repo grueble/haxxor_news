0b28ec574c1a408d24f66cff3f4e8a47e4222b11
Will Gueble <will.gueble@viget.com>
2013-06-24 13:45:03 -0600

    Remove Unnecessary Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def commentable_comments_path(commentable)
-    if commentable.is_a?(Article)
-      article_comments_path(commentable)
-    else
-      comment_comments_path(commentable)
-    end
-  end
 end
