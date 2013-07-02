98324d9c00f404ee11144ae1df8d84cd3f9c2226
Will Gueble <will.gueble@viget.com>
2013-07-02 13:47:59 -0600

    Modify Several Tests for Increased Accuracy

--- spec/features/article_list_spec.rb
+++ spec/features/article_list_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe "the article listing process" do
-  let!(:article1) { create(:article, :created_at => 10.seconds.ago, :title => "Second") }
+  let!(:article1) { create(:article, :created_at => 1.day.ago, :title => "Second") }
   let!(:article2) { create(:article, :created_at => 5.seconds.ago,  :title => "First") }
+  let(:vote) { create(:upvote, :votable => :article1) }
   
   it "lists all articles" do
     visit '/'
@@ -14,29 +15,28 @@ describe "the article listing process" do
   it "lists all articles by rating" do
     visit '/top-rated'
     
-    page.should have_content "First"
     page.should have_content "Second"
+    page.should have_content "First"
   end
   
   it "lists all articles by rating for a particular year" do
     visit "/top-rated/#{Time.now.year}"
     
-    page.should have_content "First"
     page.should have_content "Second"
+    page.should have_content "First"
   end
   
   it "lists all articles by rating for a particular month" do
     visit "/top-rated/#{Time.now.year}/#{Time.now.month}"
     
-    page.should have_content "First"
     page.should have_content "Second"
+    page.should have_content "First"
   end
   
   it "lists all articles by rating for a particular day" do
     visit "/top-rated/#{Time.now.year}/#{Time.now.month}/#{Time.now.day}"
     
     page.should have_content "First"
-    page.should have_content "Second"
   end
   
   it "lists all articles by newest" do
